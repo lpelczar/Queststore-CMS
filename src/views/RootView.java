ac4b834a8882228b07ce41ad653a14682e716d9b
lpelczar <lukepelczar@gmail.com>
2018-01-10 11:18:32 +0100

    Add displaying menu ang getting input in root view

--- src/views/RootView.java
+++ src/views/RootView.java
@@ -1,4 +1,29 @@
 package views;
 
+import java.util.Scanner;
+
 public class RootView {
+
+    public void displayMenu() {
+
+        System.out.println("Welcome to QuestStore" +
+                "\nWhat do you want to do?\n" +
+                " 1. Sign in\n" +
+                " 2. Sign up\n" +
+                " 0. Exit");
+    }
+
+    public String getUserInput() {
+        System.out.print("Choose option: ");
+        return getStringInput();
+    }
+
+    private String getStringInput() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    public void displayWrongInputMessage() {
+        System.out.println("Wrong input!");
+    }
 }
