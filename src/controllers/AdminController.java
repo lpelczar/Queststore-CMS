af57d0a41efd43d00251aba6b3fb6c6db301d1fa
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2018-01-10 14:27:46 +0100

    Add while loop in Admin controller

--- src/controllers/AdminController.java
+++ src/controllers/AdminController.java
@@ -1,8 +1,46 @@
 package controllers;
+import src.views.AdminView;
 
 public class AdminController {
+    AdminView view = new AdminView();
+
+    private boolean isRunning = true;
 
     public void start() {
-        System.out.println("Started!");
+        while (isRunning) {
+            view.handleAdminMenu();
+
+            try {
+                int option = view.askForOption();
+            }
+            catch (InputMismatchException e) {
+                System.err.println("You type wrong sign!");
+            }
+
+            if (option == 1) {
+                //Promote blank user
+                ;
+            }
+            else if (option == 2) {
+                // Edit user profile
+                ;
+            }
+            else if (option == 3) {
+                // Create new group
+                ;
+            }
+            else if (option == 4) {
+                // Edit level treshold
+                ;
+            }
+            else if (option == 5) {
+                // Show all accounts with details
+                ;
+            }
+            else if (option == 6) {
+                isRunning = false;
+            }
+        }
+
     }
 }
