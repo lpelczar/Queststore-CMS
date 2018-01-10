747243fd918afdf2fdd1e62ca3bd9e8255053827
lpelczar <lukepelczar@gmail.com>
2018-01-10 11:03:46 +0100

    Add constructor in root controller

--- src/controllers/RootController.java
+++ src/controllers/RootController.java
@@ -1,4 +1,13 @@
 package controllers;
 
 public class RootController {
+
+    private UsersDAO usersDAO;
+    private RootView rootView;
+
+    RootController() {
+
+        this.usersDAO = new UsersDAO();
+        this.rootView = new RootView();
+    }
 }
