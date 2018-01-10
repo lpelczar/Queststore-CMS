4ecc9db1cf7112dfcbba435335195e61c0640b3e
lpelczar <lukepelczar@gmail.com>
2018-01-10 14:16:54 +0100

    Implement getting user by login and password

--- src/dao/UsersDAO.java
+++ src/dao/UsersDAO.java
@@ -7,14 +7,28 @@ import java.util.List;
 
 public class UsersDAO extends AbstractDAO {
 
+    private List<User> usersList = new ArrayList<>();
+
     public UsersDAO() {}
 
     @SuppressWarnings("unchecked")
-    public List<User> getAllUsers() {
+    private void readAllUsers() {
 
-        List<User> usersList = new ArrayList<>();
+        usersList = new ArrayList<>();
         String filepath = "src/data/users.ser";
-        usersList = (ArrayList<User>) readAllData(filepath);
-        return usersList;
+        this.usersList = (ArrayList<User>) readAllData(filepath);
+    }
+
+    public User getUserByLoginAndPassword(String login, String password) {
+
+        readAllUsers();
+        User user = null;
+
+        for (User u : usersList) {
+            if (u.getLogin().equals(login) && u.getPassword().equals(password)) {
+                user = u;
+            }
+        }
+        return user;
     }
 }
