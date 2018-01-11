90a030898e55ad087cb147ff9379c7112d67792f
Vernitrax <ty21@op.pl>
2018-01-11 07:19:55 +0100

    Add start method

--- src/controllers/StudentController.java
+++ src/controllers/StudentController.java
@@ -10,4 +10,39 @@ public class StudentController {
 
     private boolean isRunning = true;
 
+    public void start() {
+        int option = 0;
+
+        while (isRunning) {
+            view.handleStudentMenu();
+
+            try {
+                option = view.askForOption();
+            }
+            catch (InputMismatchException e) {
+                System.err.println("You type wrong sign!");
+            }
+
+            if (option == 1) {
+                // See Student's backpack
+                ;
+            }
+            else if (option == 2) {
+                // Buy item
+                ;
+            }
+            else if (option == 3) {
+                // Buy item with team
+                ;
+            }
+            else if (option == 4) {
+                // See Student's level
+                ;
+            }
+            else if (option == 5) {
+                isRunning = false;
+            }
+        }
+    }
+
 }
