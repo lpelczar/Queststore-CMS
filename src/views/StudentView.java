c9bd3ebf80c5a1cae6f2e2816b5d781a75d10336
Vernitrax <ty21@op.pl>
2018-01-11 07:56:59 +0100

    Implement prepareStudentMenu

--- src/views/StudentView.java
+++ src/views/StudentView.java
@@ -1,11 +1,20 @@
 package views;
-import models.BlankUser;
+import models.Student;
 
 import java.util.*;
 
-public class MentorView {
+public class StudentView {
 
     private Map<Integer, String> menu;
     private Scanner scanner = new Scanner(System.in);
+
+    public void prepareStudentMenu() {
+        menu.put(1, "See your wallet.");
+        menu.put(2, "Buy artifact.");
+        menu.put(3, "Buy artifact with your teammates.");
+        menu.put(4, "See your level.");
+        menu.put(5, "Log out.");
+    }
+
 
 }
