2f4d760f51063afff3a0034c2a2667dcda75fe46
Vernitrax <ty21@op.pl>
2018-01-11 10:08:16 +0100

    Implement askForOption method

--- src/views/UserView.java
+++ src/views/UserView.java
@@ -4,6 +4,15 @@ import java.util.*;
 
 abstract class UserView {
 
+    protected int askForOption() throws InputMismatchException {
 
+        int option = 0;
+
+        while(!scanner.hasNextInt()) {
+            System.out.println("\nEnter option: ");
+            option = scanner.nextInt();
+        }
+        return option;
+    }
 
 }
