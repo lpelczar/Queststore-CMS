c68c7a2c418e89aa4539dec25faa3c753ae38719
Vernitrax <ty21@op.pl>
2018-01-11 06:34:32 +0100

    Add MentorView class

---
+++ src/views/MentorView.java
@@ -0,0 +1,12 @@
+package views;
+import models.BlankUser;
+
+import java.util.*;
+
+public class AdminView {
+
+    private Map<Integer, String> menu;
+    private Scanner scanner = new Scanner(System.in);
+
+    
+}
