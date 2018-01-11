79b25341ae60644220096e584ab3f4ee11115288
Vernitrax <ty21@op.pl>
2018-01-11 06:43:20 +0100

    Implement prepareMentorMenu method

--- src/views/MentorView.java
+++ src/views/MentorView.java
@@ -3,10 +3,21 @@ import models.BlankUser;
 
 import java.util.*;
 
-public class AdminView {
+public class MentorView {
 
     private Map<Integer, String> menu;
     private Scanner scanner = new Scanner(System.in);
 
-    
+    public void prepareMentorMenu() {
+        menu.put(1, "Promote blank user to student.");
+        menu.put(2, "Add new quest.");
+        menu.put(3, "Add new artifact to store.");
+        menu.put(4, "Edit quest.");
+        menu.put(5, "Edit artifact.");
+        menu.put(6, "Mark Codecooler's quest.");
+        menu.put(7, "Mark Codecooler's bought artifacts.");
+        menu.put(8, "See Codecooler's wallet.");
+        menu.put(9, "Log out.");
+    }
+
 }
