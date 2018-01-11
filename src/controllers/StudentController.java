d012e2b9c6cc22b462d0f0fd79fe3dd201dc7ad6
Vernitrax <ty21@op.pl>
2018-01-11 07:16:28 +0100

    Add StudentController class

--- src/controllers/StudentController.java
+++ src/controllers/StudentController.java
@@ -1,9 +1,13 @@
 package controllers;
 
-public class StudentController {
 
-    public void start() {
-        System.out.println("Started!");
-    }
+import views.StudentView;
+
+import java.util.InputMismatchException;
+
+public class StudentController {
+    StudentView view = new StudentView();
+
+    private boolean isRunning = true;
 
 }
