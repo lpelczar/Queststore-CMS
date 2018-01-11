a883b652e5282262179f0e1f963846cd6c78519b
lpelczar <lukepelczar@gmail.com>
2018-01-11 22:12:11 +0100

    Make the version to 0.1

--- src/views/RootView.java
+++ src/views/RootView.java
@@ -7,7 +7,7 @@ public class RootView {
     public void displayMenu() {
 
         clearConsole();
-        System.out.println("Welcome to QuestStore" +
+        System.out.println("Welcome to QuestStore v.0.1" +
                 "\nWhat do you want to do?\n" +
                 " 1. Sign in\n" +
                 " 2. Sign up\n" +
