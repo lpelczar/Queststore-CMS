368441362dc8614ccaa5c5ddde1189282fb2619c
Vernitrax <ty21@op.pl>
2018-01-11 07:53:54 +0100

    Add StudentView class

---
+++ src/views/StudentView.java
@@ -0,0 +1,11 @@
+package views;
+import models.BlankUser;
+
+import java.util.*;
+
+public class MentorView {
+
+    private Map<Integer, String> menu;
+    private Scanner scanner = new Scanner(System.in);
+
+}
