b1b480b836ae15758b507ed13520e3b3d592c75c
Vernitrax <ty21@op.pl>
2018-01-11 07:04:29 +0100

    Implemented start method

--- src/controllers/MentorController.java
+++ src/controllers/MentorController.java
@@ -10,4 +10,57 @@ public class MentorController {
 
     private boolean isRunning = true;
 
+    public void start() {
+        int option = 0;
+
+        while (isRunning) {
+            view.handleMentorMenu();
+
+            try {
+                option = view.askForOption();
+            }
+            catch (InputMismatchException e) {
+                System.err.println("You type wrong sign!");
+            }
+
+            if (option == 1) {
+                //Promote blank user to Student, add argument (list with users) to display method
+                //view.displayBlankUsers();
+                String userLogin = view.askForLoginToPromote();
+                ;
+            }
+            else if (option == 2) {
+                // Add new task
+                ;
+            }
+            else if (option == 3) {
+                // Add new item
+                ;
+            }
+            else if (option == 4) {
+                // Edit quest
+                ;
+            }
+            else if (option == 5) {
+                // Edit item
+                ;
+            }
+            else if (option == 5) {
+                // Mark Student's task
+                ;
+            }
+            else if (option == 5) {
+                // Mark Students's bought items
+                ;
+            }
+            else if (option == 5) {
+                // See Student's backpack
+                ;
+            }
+            else if (option == 6) {
+                isRunning = false;
+            }
+        }
+    }
+
 }
