5dcdbcbf09850eee3997c7251aaabdd5598ed1e3
lpelczar <lukepelczar@gmail.com>
2018-01-11 10:12:23 +0100

    Refactor reading and saving from file

--- src/dao/AbstractDAO.java
+++ src/dao/AbstractDAO.java
@@ -2,6 +2,8 @@ package dao;
 
 import services.Serializator;
 
+import java.io.File;
+
 abstract class AbstractDAO {
 
     protected void saveAllData(Object data, String filepath) {
@@ -9,6 +11,11 @@ abstract class AbstractDAO {
     }
 
     protected Object readAllData(String filepath) {
-        return Serializator.deserializeObject(filepath);
+
+        Object object = null;
+        if (new File(filepath).exists()) {
+            object = Serializator.deserializeObject(filepath);
+        }
+        return object;
     }
 }
