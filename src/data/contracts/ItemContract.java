44c69db91a16917dd2ab22c2be54f17b6b660834
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2018-01-23 13:04:05 +0100

    Add class ItemContract

--- src/data/contracts/ItemContract.java
+++ src/data/contracts/ItemContract.java
@@ -1,4 +1,15 @@
 package data.contracts;
 
 public class ItemContract {
+
+    public static final ItemEntry {
+
+        public static final String TABLE_NAME = "Item";
+        public static final String ID = "ID";
+        public static final String ITEM_NAME = "item_name";
+        public static final String DESCRIPTION = "description";
+        public static final String CATEGORY = "category";
+        public static final String PRICE = "price";
+
+    }
 }
