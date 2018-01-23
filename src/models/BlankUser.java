d5e3225e7169999259e47ff3fc25c7489927ff27
lpelczar <lukepelczar@gmail.com>
2018-01-23 18:27:39 +0100

    Add blank user model

---
+++ src/models/BlankUser.java
@@ -0,0 +1,8 @@
+package models;
+
+public class BlankUser extends User {
+
+    public BlankUser(int id, String name, String login, String email, String password, String phoneNumber, String role) {
+        super(id, name, login, email, password, phoneNumber, role);
+    }
+}
