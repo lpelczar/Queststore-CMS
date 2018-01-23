de14b4b644d4b40149894c968a96509a826447f1
lpelczar <lukepelczar@gmail.com>
2018-01-23 14:06:08 +0100

    Modify user contract and add statement

--- src/data/contracts/UserContract.java
+++ src/data/contracts/UserContract.java
@@ -1,16 +1,17 @@
 package data.contracts;
 
-class UserContract {
+public class UserContract {
 
     public static final class UserEntry {
 
         public static final String TABLE_NAME = "User";
         public static final String ID = "ID";
         public static final String NAME = "name";
+        public static final String EMAIL = "email";
         public static final String LOGIN = "login";
         public static final String PASSWORD = "password";
         public static final String PHONE_NUMBER = "phone_number";
-
+        public static final String ROLE = "role";
     }
 }
 
--- src/data/statements/UserStatement.java
+++ src/data/statements/UserStatement.java
@@ -1,4 +1,11 @@
 package data.statements;
 
+
+import data.contracts.UserContract.UserEntry;
+
 public class UserStatement {
+
+    public String selectAllUsers() {
+        return "SELECT * FROM " + UserEntry.TABLE_NAME + ";" ;
+    }
 }
