35134aa033690b959df14fe70653ba3fe8a14532
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2018-01-23 13:03:49 +0100

    Add class GroupContract

--- src/data/contracts/GroupContract.java
+++ src/data/contracts/GroupContract.java
@@ -1,4 +1,11 @@
 package data.contracts;
 
-public class GroupContract {
+public class GroupTableContract {
+
+    public static final class GroupTableEntry {
+
+        public static final String TABLE_NAME = "GroupTable";
+        public static final String ID = "ID";
+        public static final String GROUP_NAME = "group_name";
+    }
 }
