ed2e1424b335fbe55013cd387245c1e4d16ff515
lpelczar <lukepelczar@gmail.com>
2018-01-24 15:15:28 +0100

    Add sample student and mentor to fake data

--- FakeData.sql
+++ FakeData.sql
@@ -13,3 +13,35 @@ INSERT INTO User (
     "123-456-789",
     "Admin"
 );
+
+INSERT INTO User (
+    name,
+    login,
+    email,
+    password,
+    phone_number,
+    role
+) VALUES (
+    "Student",
+    "Student",
+    "student@codecool.com",
+    "student123",
+    "123-456-789",
+    "Student"
+);
+
+INSERT INTO User (
+    name,
+    login,
+    email,
+    password,
+    phone_number,
+    role
+) VALUES (
+    "Mentor",
+    "Mentor123",
+    "mentor@codecool.com",
+    "mentor123",
+    "123-456-789",
+    "Mentor"
+);
