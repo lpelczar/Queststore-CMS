32b67601f9b3ba02383236b10c90e51769ada64b
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2018-01-24 14:24:56 +0100

    Refactor query for DB, add semicolon and plus

--- src/data/statements/StudentItemStatement.java
+++ src/data/statements/StudentItemStatement.java
@@ -5,8 +5,8 @@ import data.contracts.StudentItemContract.StudentItemEntry;
 public class StudentItemStatement {
 
     public String getStudentItemsId(int student_id) {
-        return "SELECT " StudentItemEntry.ID_ITEM +
+        return "SELECT " + StudentItemEntry.ID_ITEM +
                 " FROM " + StudentItemEntry.TABLE_NAME +
-                " WHERE " + student_id + " = " + StudeItemEntry.ID_STUDENT + ";"
+                " WHERE " + student_id + " = " + StudeItemEntry.ID_STUDENT + "; ";
     }
 }
