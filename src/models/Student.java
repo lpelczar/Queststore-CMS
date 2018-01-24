91fc0886e5f01fae03aa5937ed42c1fb436eb6fb
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2018-01-24 11:59:36 +0100

    Refactor logic of Student class, delete constructor

--- src/models/Student.java
+++ src/models/Student.java
@@ -1,18 +1,12 @@
 package models;
 
-public class Student extends User {
+public class StudentData extends User {
 
-    private Level level = new Level();
-    private Inventory backpack = new Inventory();
+    private Level level;
+    private Inventory backpack;
     private Integer balance = 0;
     private String group_name;
 
-    public Student (int id, String name, String login,
-                    String email, String password,
-                    String phoneNumber, String role) {
-
-        super(id, name, login, email, password, phoneNumber, role);
-    }
 
     public Level getLevel() {
         return level;
