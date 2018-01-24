dc66ec1845eda605f744194748db61f5508fa7e2
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2018-01-24 15:10:20 +0100

    Resolve bug with type in WHERE statement

--- src/data/statements/StudentItemStatement.java
+++ src/data/statements/StudentItemStatement.java
@@ -7,6 +7,6 @@ public class StudentItemStatement {
     public String getStudentItemsId(int student_id) {
         return "SELECT " + StudentItemEntry.ID_ITEM +
                 " FROM " + StudentItemEntry.TABLE_NAME +
-                " WHERE " + student_id + " = " + StudeItemEntry.ID_STUDENT + "; ";
+                " WHERE " + student_id + " = " + StudentItemEntry.ID_STUDENT + "; ";
     }
 }
