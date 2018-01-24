8f56aaba464387853c204e703fa8cd5cf90df5ef
lpelczar <lukepelczar@gmail.com>
2018-01-24 10:36:13 +0100

    Delete unused classes

--- src/data/contracts/MentorContract.java
+++
@@ -1,9 +0,0 @@
-package data.contracts;
-
-public class MentorContract {
-
-    public static final class MentorEntry {
-
-        public static final String ROLE = "Mentor";
-    }
-}
--- src/models/BlankUser.java
+++
@@ -1,8 +0,0 @@
-package models;
-
-public class BlankUser extends User {
-
-    public BlankUser(int id, String name, String login, String email, String password, String phoneNumber, String role) {
-        super(id, name, login, email, password, phoneNumber, role);
-    }
-}
