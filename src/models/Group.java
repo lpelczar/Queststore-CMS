7d312620abfaae9622be55115c7d366f652cb70e
lpelczar <lukepelczar@gmail.com>
2018-01-24 10:44:49 +0100

    Modify Group model

--- src/models/Group.java
+++ src/models/Group.java
@@ -1,51 +1,14 @@
 package models;
-import java.io.Serializable;
-import java.util.*;
 
-public class Group implements Serializable {
-    private List<Team> group;
-    private final int ID;
-    private static int nextID = 1;
+public class Group {
+
     private String groupName;
-
 
     public Group(String name) {
         this.groupName = name;
-        this.ID = nextID;
-        nextID++;
-    }
-
-    public boolean addTeam(Team team) {
-        if (!group.contains(team)) {
-            group.add(team);
-            return true;
-        }
-        return false;
-    }
-
-    public boolean removeTeam(Team team) {
-        if (group.contains(team)) {
-            group.remove(team);
-            return true;
-        }
-        return false;
-    }
-
-    public int getID() {
-        return ID;
-    }
-
-    public List<Team> getGroup() {
-        return group;
     }
 
     public String getGroupName() {
         return groupName;
     }
-
-    public void setGroupName(String groupName) {
-        this.groupName = groupName;
-    }
-
-
 }
