ef5535389cc1f09b89422297fe1586b75ad1fe81
lpelczar <lukepelczar@gmail.com>
2018-01-24 11:54:11 +0100

    Add setters to user

--- src/models/User.java
+++ src/models/User.java
@@ -68,4 +68,20 @@ public class User {
     public void setRole(String role) {
         this.role = role;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
