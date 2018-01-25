4449b7829ce7513bfba9f78d0581cb2a07930fbf
lpelczar <lukepelczar@gmail.com>
2018-01-25 10:51:14 +0100

    Add jdbc dependency

--- compilerun.sh
+++ compilerun.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-javac -d bin -sourcepath src/  src/main/Application.java 
+javac -d bin -sourcepath src/ -cp lib/sqlite-jdbc-3.21.0.jar src/main/Application.java 
 java  -classpath bin:./lib/sqlite-jdbc-3.21.0.jar main.Application
