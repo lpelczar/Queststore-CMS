8ba028b7719a03cdddb32848806243227f09976a
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2018-01-25 10:50:20 +0100

    Add possibilty to add new items in DB by Mentor

--- src/controllers/MentorController.java
+++ src/controllers/MentorController.java
@@ -2,39 +2,42 @@ package controllers;
 
 
 import java.util.InputMismatchException;
+import dao.DbItemDAO;
 
 public class MentorController {
-//    MentorView view = new MentorView();
-//
-//    private boolean isRunning = true;
+    MentorView view = new MentorView();
 
-//    public void start(){
-//
-//        while (isRunning) {
-//            view.handleMentorMenu();
-//            int option = 0;
-//            try {
-//                option = view.askForOption();
-//            } catch (InputMismatchException e) {
-//                System.err.println("You type wrong sign!");
-//            }
-//
-//            if (option == 1) {
-//            } else if (option == 2) {
-//                createTask();
-//            } else if (option == 3) {
-//                createItem();
-//            } else if (option == 4) {
-//            } else if (option == 5) {
-//            } else if (option == 6) {
-//            } else if (option == 7) {
-//            } else if (option == 8) {
-//            } else if (option == 9) {
-//                isRunning = false;
-//            }
-//        }
-//    }
-//
+    private boolean isRunning = true;
+
+    public void start(){
+
+        while (isRunning) {
+            view.handleMentorMenu();
+
+            int option = 0;
+
+            try {
+                option = view.askForOption();
+            } catch (InputMismatchException e) {
+                System.err.println("You type wrong sign!");
+            }
+
+            if (option == 1) {
+            } else if (option == 2) {
+                createTask();
+            } else if (option == 3) {
+                createItem();
+            } else if (option == 4) {
+            } else if (option == 5) {
+            } else if (option == 6) {
+            } else if (option == 7) {
+            } else if (option == 8) {
+            } else if (option == 9) {
+                isRunning = false;
+            }
+        }
+    }
+
 //    public void createTask() {
 //
 //        TaskDAO taskdao = new TaskDAO();
@@ -55,18 +58,37 @@ public class MentorController {
 //        taskdao.addTask(task);
 //
 //    }
-//
-//    public void createItem() {
-//
-//        view.displayCreatingItem();
-//        String name = view.askForInput();
-//        int price = 0;
-//        try {
-//            price = view.askForPrice();
-//        }
-//        catch (InputMismatchException e) {
-//            System.err.println("You type wrong sign!");
-//        }
-//        String description = view.askForInput();
-//    }
+
+    public void createItem() {
+        DbItemDAO dbItemDAO = new DbItemDAO();
+        view.clearConsole();
+
+        view.displayCreatingItem();
+        String name = view.askForInput();
+        int price priceCheck();
+        String description = view.askForInput();
+        String category = askForItemCategory();
+
+        Item item = new Item(name, price, description, category);
+
+        if (dbItemDAO.addItem(item)) {
+            view.displayOperationSuccesfull();
+        }
+        else {
+            view.displayOperationFailed();
+        }
+
+    }
+
+    public int priceCheck() {
+        int price;
+
+        try {
+            while(!scanner.hasNextInt()) {
+                price = askForPrice();
+            }
+        } catch (InputMismatchException e) {
+            System.err.println("You type wrong sign!");
+        }
+    }
 }
