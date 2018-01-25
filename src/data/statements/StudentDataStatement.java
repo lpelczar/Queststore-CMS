c108f92057529a447bcd57641a338469cb6d74e2
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2018-01-25 23:34:13 +0100

    Add query for update student data

--- src/data/statements/StudentDataStatement.java
+++ src/data/statements/StudentDataStatement.java
@@ -1,6 +1,9 @@
 package data.statements;
 
 import data.contracts.StudentDataContract.StudentDataEntry;
+import data.contracts.UserContract.UserEntry;
+import models.StudentData;
+import models.User;
 
 public class StudentDataStatement {
 
@@ -8,5 +11,22 @@ public class StudentDataStatement {
         return "SELECT " + StudentDataEntry.LEVEL +
                 " FROM " + StudentDataEntry.TABLE_NAME +
                 " WHERE " + student_id + " = " + StudentDataEntry.ID_USER + "; ";
+    }
+
+    public static String createStudentData(StudentData student, User user) {
+        // For test, add same foreign key for team and Id user. After team implementation change it.
+        return "INSERT INTO " + StudentDataEntry.TABLE_NAME + " ( " +
+                StudentDataEntry.ID_USER + ", " +
+                StudentDataEntry.ID_GROUP + ", " +
+                StudentDataEntry.TEAM_NAME + ", " +
+                StudentDataEntry.LEVEL + ", " +
+                StudentDataEntry.BALANCE + ") " +
+                "VALUES (" +
+                // how i can get user id here... + ", "
+                user.getId() + ", " +
+                user.getId() + ", \'" +
+                student.getTeamName() + "\', \'" +
+                student.getLevel() + "\'," +
+                student.getBalance() + "); ";
     }
 }
