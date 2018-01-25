bcd9674ff68b15457327f0b765f7c8c5d815453c
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2018-01-25 17:44:18 +0100

    Add field team name, getter

--- src/models/StudentData.java
+++ src/models/StudentData.java
@@ -4,6 +4,7 @@ public class StudentData {
 
     private String level;
     private String group_name;
+    private String team_name;
     private int exp;
     private int balance;
 
@@ -16,6 +17,10 @@ public class StudentData {
     public Integer getBalance() {
         return balance;
     }
+    
+    public String getGroupName() { return group_name; }
+
+    public String getTeamName() { return team_name; }
 
     public void setLevel(String level) {
         this.level = level;
