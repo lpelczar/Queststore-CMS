652020cb087282e2436d7171dc444be362cbd860
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2018-01-25 23:32:17 +0100

    Add method for update query in DB, StudentDataDAO/DbStudentDataDAO

--- src/dao/StudentDataDAO.java
+++ src/dao/StudentDataDAO.java
@@ -1,9 +1,11 @@
 package dao;
 
 import models.StudentData;
+import models.User;
 
 public interface StudentDataDAO {
 
     StudentData getStudentLevelBy(int student_id);
+    boolean add(StudentData student, User user);
 
 }
