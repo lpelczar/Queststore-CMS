9cc2da52d2243dffe7255f6111042d56ddcabc02
lpelczar <lukepelczar@gmail.com>
2018-02-03 15:50:29 +0100

    Modify experience levels dao to reflect sql injection

--- src/main/java/com/example/queststore/dao/DbExpLevelsDAO.java
+++ src/main/java/com/example/queststore/dao/DbExpLevelsDAO.java
@@ -5,6 +5,7 @@ import com.example.queststore.data.contracts.ExperienceLevelEntry;
 import com.example.queststore.data.statements.ExperienceLevelStatement;
 import com.example.queststore.models.ExpLevel;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -16,16 +17,18 @@ public class DbExpLevelsDAO extends DbHelper implements ExpLevelsDAO {
 
     @Override
     public List<ExpLevel> getAll() {
-        String statement = expStatement.selectAllExpLevels();
+        String sqlStatement = expStatement.selectAllExpLevels();
 
         List<ExpLevel> levels = new ArrayList<>();
         try {
+            PreparedStatement statement = getPreparedStatement(sqlStatement);
             ResultSet resultSet = query(statement);
             while (resultSet.next())
                 levels.add(new ExpLevel(
                         resultSet.getString(ExperienceLevelEntry.NAME),
                         resultSet.getInt(ExperienceLevelEntry.LEVEL)));
             resultSet.close();
+            statement.close();
         } catch (SQLException e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
         } finally {
