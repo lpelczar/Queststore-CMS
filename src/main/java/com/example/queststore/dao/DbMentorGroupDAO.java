f5c6f456c79896861ed62b075807b1b6cd52ee78
lpelczar <lukepelczar@gmail.com>
2018-02-04 14:03:12 +0100

    Modify adding and deleting group mentor connections

--- src/main/java/com/example/queststore/dao/DbMentorGroupDAO.java
+++ src/main/java/com/example/queststore/dao/DbMentorGroupDAO.java
@@ -3,6 +3,9 @@ package com.example.queststore.dao;
 import com.example.queststore.data.DbHelper;
 import com.example.queststore.data.statements.MentorGroupStatement;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class DbMentorGroupDAO extends DbHelper implements MentorGroupDAO {
 
     private MentorGroupStatement mentorGroupStatement = new MentorGroupStatement();
@@ -10,13 +13,29 @@ public class DbMentorGroupDAO extends DbHelper implements MentorGroupDAO {
     @Override
     public boolean add(int groupID, int mentorID) {
 
-        String statement = mentorGroupStatement.insertConnectionStatement(groupID, mentorID);
+        String sqlStatement = mentorGroupStatement.insertConnectionStatement();
+        PreparedStatement statement = null;
+        try {
+            statement = getPreparedStatement(sqlStatement);
+            statement.setInt(1, groupID);
+            statement.setInt(2, mentorID);
+        } catch (SQLException e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
         return update(statement);
     }
 
     @Override
     public boolean delete(int groupID, int mentorID) {
-        String statement = mentorGroupStatement.deleteConnectionStatement(groupID, mentorID);
+        String sqlStatement = mentorGroupStatement.deleteConnectionStatement();
+        PreparedStatement statement = null;
+        try {
+            statement = getPreparedStatement(sqlStatement);
+            statement.setInt(1, groupID);
+            statement.setInt(2, mentorID);
+        } catch (SQLException e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
         return update(statement);
     }
 }
