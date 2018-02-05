be5c62ebc00a59d31f6a17e9b04f457ff6d5829c
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2018-02-05 14:21:02 +0100

    Add inserting new students records to DB

--- InsertFakeData.sql
+++ InsertFakeData.sql
@@ -22,12 +22,12 @@ INSERT INTO users (
     phone_number,
     role
 ) VALUES (
-    "Student",
-    "Student",
-    "student@codecool.com",
-    "student123",
-    "123-456-798",
-    "Student"
+    "Mentor",
+    "Mentor",
+    "mentor@codecool.com",
+    "mentor123",
+    "123-456-799",
+    "Mentor"
 );
 
 INSERT INTO users (
@@ -38,12 +38,97 @@ INSERT INTO users (
     phone_number,
     role
 ) VALUES (
-    "Mentor",
-    "Mentor",
-    "mentor@codecool.com",
-    "mentor123",
-    "123-456-799",
-    "Mentor"
+    "Student",
+    "Student",
+    "student@codecool.com",
+    "student123",
+    "123-456-798",
+    "Student"
 );
+
+INSERT INTO students_data VALUES (
+    3,
+    1,
+    "not assign yet",
+    "beginner",
+    1000,
+    0
+);
+
+INSERT INTO users (
+    name,
+    login,
+    email,
+    password,
+    phone_number,
+    role
+) VALUES (
+    "nikodem",
+    "nikodem",
+    "nikodem@codecool.com",
+    "nikodem123",
+    "123-456-000",
+    "Student"
+);
+
+INSERT INTO students_data VALUES (
+    4,
+    1,
+    "not assign yet",
+    "beginner",
+    1000,
+    0
+);
+
+INSERT INTO users (
+    name,
+    login,
+    email,
+    password,
+    phone_number,
+    role
+) VALUES (
+    "ryszard",
+    "ryszard",
+    "ryszard@codecool.com",
+    "ryszard123",
+    "123-456-556",
+    "Student"
+);
+
+INSERT INTO students_data VALUES (
+    5,
+    1,
+    "not assign yet",
+    "beginner",
+    1000,
+    0
+);
+
+INSERT INTO users (
+    name,
+    login,
+    email,
+    password,
+    phone_number,
+    role
+) VALUES (
+    "janusz",
+    "janusz",
+    "janusz@codecool.com",
+    "janusz123",
+    "123-456-223",
+    "Student"
+);
+
+INSERT INTO students_data VALUES (
+    6,
+    1,
+    "not assign yet",
+    "beginner",
+    1000,
+    0
+);
+
 
 INSERT INTO groups (group_name) VALUES ("No group");
