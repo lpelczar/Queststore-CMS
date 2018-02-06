0be9b443202839fc4cd3504f7290c66a507db32d
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2018-02-06 14:21:17 +0100

    Add methods which get info about what items contain student from DB, DbStudentItemDAO class/ StudentItemDAO interface

--- src/main/java/com/example/queststore/dao/DbStudentItemDAO.java
+++ src/main/java/com/example/queststore/dao/DbStudentItemDAO.java
@@ -2,9 +2,14 @@ package com.example.queststore.dao;
 
 import com.example.queststore.data.DbHelper;
 import com.example.queststore.data.PreparedStatementCreator;
+import com.example.queststore.data.contracts.StudentItemEntry;
 import com.example.queststore.data.statements.StudentItemStatement;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DbStudentItemDAO extends DbHelper implements StudentItemDAO {
 
@@ -16,5 +21,28 @@ public class DbStudentItemDAO extends DbHelper implements StudentItemDAO {
         String sqlStatement = studentItemStatement.addStudentItemConnection();
         PreparedStatement statement = psc.getPreparedStatementBy(student_id, item_id, sqlStatement);
         return update(statement);
+    }
+
+    @Override
+    public List<Integer> getStudentsItemsBy(int studentID) {
+        String sqlStatement = studentItemStatement.getStudentsItems();
+        List<Integer> studentsItems = new ArrayList<>();
+
+        try {
+            PreparedStatement statement = psc.getPreparedStatementBy(studentID, sqlStatement);
+            ResultSet resultSet = query(statement);
+
+            while (resultSet.next()) {
+                studentsItems.add(resultSet.getInt(StudentItemEntry.ID_ITEM));
+            }
+
+            resultSet.close();
+            statement.close();
+        } catch (SQLException e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        } finally {
+            closeConnection();
+        }
+        return studentsItems;
     }
 }
--- src/main/java/com/example/queststore/dao/StudentItemDAO.java
+++ src/main/java/com/example/queststore/dao/StudentItemDAO.java
@@ -1,6 +1,9 @@
 package com.example.queststore.dao;
 
+import java.util.List;
+
 public interface StudentItemDAO {
 
     boolean add(int student_id, int item_id);
+    List<Integer> getStudentsItemsBy(int studentID);
 }
