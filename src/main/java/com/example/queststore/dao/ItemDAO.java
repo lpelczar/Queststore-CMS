3048852274586ac58e9b0f541b7addd561797595
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2018-02-06 12:58:11 +0100

    Add method for get items by category from DB, Item DAOs class and interface

--- src/main/java/com/example/queststore/dao/ItemDAO.java
+++ src/main/java/com/example/queststore/dao/ItemDAO.java
@@ -7,9 +7,9 @@ import java.util.List;
 public interface ItemDAO {
 
     List<Item> getItemsByStudentId(int student_id);
+    List<Item> getItemsByCategory(String category);
     List<Item> getAllItems();
     Item getItemById(int id);
     boolean addItem(Item item);
     boolean updateItem(Item item);
-
 }
