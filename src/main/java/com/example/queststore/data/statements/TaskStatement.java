447fd444a3181f1d1bb5405476b43cb1c36d4ab9
lpelczar <lukepelczar@gmail.com>
2018-02-06 11:18:33 +0100

    Add getting task by name statement

--- src/main/java/com/example/queststore/data/statements/TaskStatement.java
+++ src/main/java/com/example/queststore/data/statements/TaskStatement.java
@@ -1,4 +1,11 @@
 package com.example.queststore.data.statements;
 
+import com.example.queststore.data.contracts.TaskEntry;
+
 public class TaskStatement {
+
+    public String selectTaskByName() {
+        return "SELECT * FROM " + TaskEntry.TABLE_NAME +
+                " WHERE " + TaskEntry.NAME + " = ?;" ;
+    }
 }
