941d8270da526b18b7e1157bf4662da006cf3473
lpelczar <lukepelczar@gmail.com>
2018-02-07 16:32:00 +0100

    Implement marking item as used and modify adding item query

--- src/main/java/com/example/queststore/data/statements/StudentItemStatement.java
+++ src/main/java/com/example/queststore/data/statements/StudentItemStatement.java
@@ -8,12 +8,20 @@ public class StudentItemStatement {
     public String addStudentItemConnection() {
         return "INSERT INTO " + StudentItemEntry.TABLE_NAME + " ( " +
                 StudentItemEntry.ID_STUDENT + ", " +
-                StudentItemEntry.ID_ITEM + " ) " +
-                "VALUES (?,?); ";
+                StudentItemEntry.ID_ITEM + ", " +
+                StudentItemEntry.IS_USED + " ) " +
+                "VALUES (?,?,?); ";
     }
 
     public String getStudentsItems() {
         return "SELECT " + StudentItemEntry.ID_ITEM + " FROM " + StudentItemEntry.TABLE_NAME +
                 " WHERE "  + StudentItemEntry.ID_STUDENT + " = ?; ";
+    }
+
+    public String markItemAsUsed() {
+        return "UPDATE " + StudentItemEntry.TABLE_NAME + " SET " +
+                StudentItemEntry.IS_USED + " = " + StudentItemEntry.IS_USED_VALUE +
+                " WHERE " + StudentItemEntry.ID_STUDENT + " = ? AND " +
+                StudentItemEntry.ID_ITEM + " = ?; ";
     }
 }
