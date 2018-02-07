fde5625ccc614eab692cdadfd57e5616138118d6
lpelczar <lukepelczar@gmail.com>
2018-02-07 17:31:19 +0100

    Add basic and extra field to task entry.

--- src/main/java/com/example/queststore/data/contracts/TaskEntry.java
+++ src/main/java/com/example/queststore/data/contracts/TaskEntry.java
@@ -1,12 +1,12 @@
 package com.example.queststore.data.contracts;
 
 public class TaskEntry {
-
-        public static final String TABLE_NAME = "tasks";
-        public static final String ID = "ID";
-        public static final String NAME = "name";
-        public static final String POINTS = "points";
-        public static final String DESCRIPTION = "description";
-        public static final String CATEGORY = "category";
-
+    public static final String TABLE_NAME = "tasks";
+    public static final String ID = "ID";
+    public static final String NAME = "name";
+    public static final String POINTS = "points";
+    public static final String DESCRIPTION = "description";
+    public static final String CATEGORY = "category";
+    public static final String BASIC_TASK = "Basic";
+    public static final String EXTRA_TASK = "Extra";
 }
