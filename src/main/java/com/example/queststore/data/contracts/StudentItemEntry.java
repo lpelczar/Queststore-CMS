6723d7f8a0497e3b820bf04663d9dcadbbd7f667
lpelczar <lukepelczar@gmail.com>
2018-02-08 09:39:53 +0100

    Modify is used constant

--- src/main/java/com/example/queststore/data/contracts/StudentItemEntry.java
+++ src/main/java/com/example/queststore/data/contracts/StudentItemEntry.java
@@ -7,5 +7,5 @@ public class StudentItemEntry {
     public static final String ID_ITEM = "id_item";
     public static final String IS_USED = "is_used";
     public static final int IS_USED_VALUE = 1;
-    public static final int IS_NOT_USED_VALUE = 1;
+    public static final int IS_NOT_USED_VALUE = 0;
 }
