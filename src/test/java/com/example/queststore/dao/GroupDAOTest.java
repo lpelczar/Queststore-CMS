c15d111b8070c97e9cd956e9f872103c4fc1fd87
lpelczar <lukepelczar@gmail.com>
2018-03-07 14:03:28 +0100

    Add test checking adding class

---
+++ src/test/java/com/example/queststore/dao/GroupDAOTest.java
@@ -0,0 +1,34 @@
+package com.example.queststore.dao;
+
+import com.example.queststore.data.DbHelper;
+import com.example.queststore.models.Group;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GroupDAOTest {
+
+    private GroupDAO groupDAO;
+
+    @BeforeEach
+    public void before() throws IOException {
+        String testDbPath = "testDb.db";
+        Files.deleteIfExists(new File(testDbPath).toPath());
+        DbHelper.setDatabasePath(testDbPath);
+        new DbHelper().createDatabase();
+        this.groupDAO = new DbGroupDAO();
+    }
+
+    @Test
+    public void whenAddThenGroupIsAddedToDb() {
+        Group expected = new Group(1,"Group1");
+        this.groupDAO.add(expected);
+        Group result = this.groupDAO.getByName(expected.getGroupName());
+        assertEquals(expected, result);
+    }
+}
