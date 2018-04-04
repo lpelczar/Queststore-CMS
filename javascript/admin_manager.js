ab26c3e6c35b0e5c47da774c8f6b758c4ee0fe36
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2018-04-04 23:02:19 +0200

    Add javascript tablesEvent to external file

--- javascript/admin_manager.js
+++ javascript/admin_manager.js
@@ -7,3 +7,33 @@ function displayWindow() {
         window.style.display = "none";
     }
 }
+
+function tablesEvent() {
+    let mentor_table = document.getElementById('mentors-table');
+    let group_table = document.getElementById('groups-table');
+    let content = document.getElementsByClassName('content-admin')[0];
+
+    mentor_table.onclick = function() {
+        content.style.justifyContent = "center";
+        mentor_table.style.position = "absolute";
+        mentor_table.style.width = "70%";
+        group_table.style.display = "none";
+
+        mentor_table.onclick = function () {
+            window.location.href = "admin_manager.html";
+        }
+    };
+
+    group_table.onclick = function () {
+        content.style.justifyContent = "center";
+        group_table.style.position = "absolute";
+        group_table.style.width = "70%";
+        mentor_table.style.display = "none";
+
+
+        group_table.onclick = function () {
+            window.location.href = "admin_manager.html";
+        }
+
+    }
+}
