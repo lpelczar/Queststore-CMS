e79a3385cf1a56f21ac539bdb09e2ff90825189c
lpelczar <lukepelczar@gmail.com>
2018-04-16 13:45:43 +0200

    Add root handler to app

---
+++ src/main/java/handlers/RootHandler.java
@@ -0,0 +1,17 @@
+package handlers;
+
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class RootHandler {
+
+    public void start() throws IOException {
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(7000), 0);
+        server.createContext("/login", new LoginHandler());
+        server.setExecutor(null);
+        server.start();
+    }
+}
