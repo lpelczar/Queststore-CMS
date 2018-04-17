aa7de36fc3c7e586aeed353951d987eb629baa45
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2018-04-17 13:25:40 +0200

    Add admin route

--- src/main/java/handlers/RootHandler.java
+++ src/main/java/handlers/RootHandler.java
@@ -12,6 +12,7 @@ public class RootHandler {
         HttpServer server = HttpServer.create(new InetSocketAddress(7000), 0);
         server.createContext("/login", new LoginHandler());
         server.createContext("/static", new StaticHandler());
+        server.createContext("/admin", new AdminHandler());
         server.setExecutor(null);
         server.start();
     }
