fa13e7713d93677c4ce1c6f8f211c6629490de91
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2018-04-17 13:24:04 +0200

    Add field with assigned mentor id to group

--- src/main/java/model/Group.java
+++ src/main/java/model/Group.java
@@ -4,10 +4,12 @@ public class Group {
 
     private int groupId;
     private String groupName;
+    private int mentor_id;
 
-    public Group(int groupId, String groupName) {
+    public Group(int groupId, String groupName, int mentor_id) {
         this.groupId = groupId;
         this.groupName = groupName;
+        this.mentor_id = mentor_id;
     }
 
     public String getGroupName() {
