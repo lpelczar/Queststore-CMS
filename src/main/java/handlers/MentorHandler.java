6569744e3a0a79a11e8e5cad78c23f04250e6619
lpelczar <lukepelczar@gmail.com>
2018-04-18 11:03:03 +0200

    Implement displaying login page when no cookie

--- src/main/java/handlers/MentorHandler.java
+++ src/main/java/handlers/MentorHandler.java
@@ -1,17 +1,58 @@
 package handlers;
 
-import com.google.common.io.Resources;
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import dao.LoginDAO;
+import dao.SqliteLoginDAO;
+import data.sessiondatabase.Session;
+import data.sessiondatabase.SessionDAO;
+import data.sessiondatabase.SqliteSessionDAO;
+import model.database.User;
 
 import java.io.IOException;
-import java.net.URL;
+import java.net.HttpCookie;
 
 public class MentorHandler implements HttpHandler {
 
+    private SessionDAO sessionDAO = new SqliteSessionDAO();
+    private LoginDAO loginDAO = new SqliteLoginDAO();
+
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
-        URL fileURL = Resources.getResource("static/mentor_manager.html");
-        StaticHandler.sendFile(httpExchange, fileURL);
+
+        String method = httpExchange.getRequestMethod();
+        HttpCookie cookie;
+
+        if (method.equals("GET")) {
+            String sessionCookie = httpExchange.getRequestHeaders().getFirst("Cookie");
+            if (sessionCookie != null) {
+                cookie = HttpCookie.parse(sessionCookie).get(0);
+                if (sessionDAO.getById(cookie.getValue()) != null) {
+                    showMentorPage(httpExchange, cookie);
+                    return;
+                }
+            }
+            redirectToLogin(httpExchange);
+        }
+    }
+
+    private void redirectToLogin(HttpExchange httpExchange) throws IOException {
+        Headers headers = httpExchange.getResponseHeaders();
+        String redirect = "/login";
+        headers.add("Location", redirect);
+        httpExchange.sendResponseHeaders(301, -1);
+    }
+
+    private void showMentorPage(HttpExchange httpExchange, HttpCookie cookie) {
+
+        String sessionId = cookie.getValue();
+        Session session = sessionDAO.getById(sessionId);
+        int userId = session.getUserId();
+        User user = loginDAO.getById(userId);
+
+        String path = httpExchange.getRequestURI().getPath();
+        System.out.println("Path: " + path);
+
     }
 }
