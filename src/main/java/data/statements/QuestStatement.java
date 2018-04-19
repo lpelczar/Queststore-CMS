3f8892028772cc226f3398c8773805fea30b4b22
lpelczar <lukepelczar@gmail.com>
2018-04-19 10:52:24 +0200

    Implement getting all quests and getting by id in dao

---
+++ src/main/java/data/statements/QuestStatement.java
@@ -0,0 +1,12 @@
+package data.statements;
+
+public class QuestStatement {
+
+    public String selectAllQuests() {
+        return "SELECT * FROM quests;";
+    }
+
+    public String selectQuestById() {
+        return "SELECT * FROM quests WHERE quest_id = ?; ";
+    }
+}
