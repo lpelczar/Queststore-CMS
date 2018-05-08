c5fc0dc96dfb5ca4de6a5948674c6dd3ac0dec37
lpelczar <lukepelczar@gmail.com>
2018-05-08 10:25:12 +0200

    Handle registration

--- src/main/java/com/example/queststore/controllers/web/RegistrationHandler.java
+++ src/main/java/com/example/queststore/controllers/web/RegistrationHandler.java
@@ -1,15 +1,20 @@
 package com.example.queststore.controllers.web;
 
 import com.example.queststore.dao.UserDAO;
+import com.example.queststore.data.contracts.UserEntry;
+import com.example.queststore.models.User;
 import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import org.jtwig.JtwigModel;
+import org.jtwig.JtwigTemplate;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.URL;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.List;
 
 class RegistrationHandler implements HttpHandler {
 
@@ -27,18 +32,65 @@ class RegistrationHandler implements HttpHandler {
 
         String method = httpExchange.getRequestMethod();
         if (method.equals(GET_METHOD)) {
-            sendRegistrationPage(httpExchange);
+            sendPage(httpExchange, "static/register.html");
         }
 
         if (method.equals(POST_METHOD)) {
             String formData = getFormData(httpExchange);
-            System.out.println(formData);
-            // handleRegistration(httpExchange, formData);
+            handleRegistration(httpExchange, formData);
         }
     }
 
-    private void sendRegistrationPage(HttpExchange httpExchange) throws IOException {
-        URL url = Resources.getResource("static/register.html");
+    private void handleRegistration(HttpExchange httpExchange, String formData) throws IOException {
+        System.out.println(formData);
+
+        User user = parseRegistrationData(formData);
+
+        if (userDAO.add(user)) {
+            sendSuccessPage(httpExchange, user);
+        } else {
+            sendPage(httpExchange, "static/registrationerror.html");
+        }
+    }
+
+    private void sendSuccessPage(HttpExchange httpExchange, User user) throws IOException {
+        JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/registration_success.twig");
+        JtwigModel model = JtwigModel.newModel();
+        model.with("userLogin", user.getLogin());
+        model.with("userName", user.getName());
+        model.with("userEmail", user.getEmail());
+        model.with("userPhoneNumber", user.getPhoneNumber());
+        sendResponse(httpExchange, template.render(model));
+    }
+
+    private void sendResponse(HttpExchange httpExchange, String response) throws IOException {
+        httpExchange.sendResponseHeaders(200, response.length());
+        OutputStream os = httpExchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+    }
+
+    private User parseRegistrationData(String formData) throws UnsupportedEncodingException {
+
+        final int LOGIN_INDEX = 0;
+        final int PASSWORD_INDEX = 1;
+        final int NAME_INDEX = 2;
+        final int EMAIL_INDEX = 3;
+        final int PHONE_INDEX = 4;
+        final int VALUE_INDEX = 1;
+
+        String[] pairs = formData.split("&");
+        List<String> values = new ArrayList<>();
+        for(String pair : pairs){
+            String[] keyValue = pair.split("=");
+            values.add(URLDecoder.decode(keyValue[VALUE_INDEX], Charsets.UTF_8.displayName()));
+        }
+        return new User(values.get(NAME_INDEX), values.get(LOGIN_INDEX), values.get(EMAIL_INDEX),
+                values.get(PASSWORD_INDEX), values.get(PHONE_INDEX), UserEntry.BLANK_USER_ROLE);
+    }
+
+    private void sendPage(HttpExchange httpExchange, String path) throws IOException {
+        URL url = Resources.getResource(path);
         StaticHandler.sendFile(httpExchange, url);
     }
 
