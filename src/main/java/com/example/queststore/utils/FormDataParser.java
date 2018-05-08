c9aa4a06822ef8bfd18e2569d1d82113d801fc84
lpelczar <lukepelczar@gmail.com>
2018-05-08 14:16:38 +0200

    Add form data parser class

---
+++ src/main/java/com/example/queststore/utils/FormDataParser.java
@@ -0,0 +1,22 @@
+package com.example.queststore.utils;
+
+import com.google.common.base.Charsets;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FormDataParser {
+
+    public List<String> parse(String formData) throws UnsupportedEncodingException {
+        final int VALUE_INDEX = 1;
+        String[] pairs = formData.split("&");
+        List<String> values = new ArrayList<>();
+        for (String pair : pairs) {
+            String[] keyValue = pair.split("=");
+            values.add(URLDecoder.decode(keyValue[VALUE_INDEX], Charsets.UTF_8.displayName()));
+        }
+        return values;
+    }
+}
