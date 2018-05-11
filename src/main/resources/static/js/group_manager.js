8ba6e5c2ccaef8b71b8dd351ebff9588e3e66243
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2018-05-11 09:41:21 +0200

    Add divs with separate mentors to pick for assign to group

--- src/main/resources/static/js/group_manager.js
+++ src/main/resources/static/js/group_manager.js
@@ -2,3 +2,8 @@ function changeSizeOfMainTable() {
     var main_table = document.getElementById("row-table");
     main_table.style.height = "90%";
 }
+
+function prepareLinksForUpdateGroup(group, mentor_id) {
+    var text = "";
+
+}
